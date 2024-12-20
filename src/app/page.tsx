f2503d8a0ba27d33bd99404bb7cdb6071bf8dbd3
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-12-20 16:55:37 +0100

    Add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ const Orb = () => {
 
   return (
     <motion.svg
-      className="absolute -inset-[25vw] size-[50vw] mix-blend-color-dodge"
+      className="absolute -inset-[12.5vw] size-[25vw] mix-blend-hard-light dark:mix-blend-color-dodge"
       width="458.751"
       height="447.742"
       viewBox="0 0 121.378 118.465"
@@ -86,7 +86,7 @@ const Orb = () => {
       </defs>
       <g
         transform="translate(-25.254 -137.364)"
-        fill-opacity=".25"
+        fill-opacity=".16"
         filter="url(#a)"
       >
         <circle cx="86.244" cy="187.423" r="20.862" fill="#00aaa8" />
@@ -150,19 +150,19 @@ export default function Home() {
 
       <main className="relative z-10 flex min-h-[100dvh] flex-col items-center justify-center p-3 lg:p-10">
         <div className="mb-28 mt-auto flex items-center gap-8 lg:mb-56 lg:gap-16">
-          <Logo className="size-20 stroke-neutral-50 lg:size-32" />
+          <Logo className="size-20 stroke-neutral-950 lg:size-32 dark:stroke-neutral-50" />
 
           <div className="flex flex-col gap-3 lg:gap-6">
-            <h1 className="text-3xl text-neutral-50 lg:text-6xl">
+            <h1 className="text-3xl text-neutral-950 lg:text-6xl dark:text-neutral-50">
               Kamil Pyszkowski
             </h1>
-            <span className="text-2xl text-neutral-400 lg:text-4xl">
+            <span className="text-2xl text-neutral-600 lg:text-4xl dark:text-neutral-400">
               Software Engineer
             </span>
           </div>
         </div>
 
-        <p className="mb-16 text-xl text-neutral-50 lg:mb-32 lg:text-2xl">
+        <p className="mb-16 text-xl text-neutral-950 lg:mb-32 lg:text-2xl dark:text-neutral-50">
           Coming soon
           <motion.span
             className="mx-2"
@@ -182,14 +182,14 @@ export default function Home() {
           </motion.span>
         </p>
 
-        <ul className="group flex text-neutral-50">
+        <ul className="group flex text-neutral-950 dark:text-neutral-50">
           {SOCIALS.map(({ name, icon: Icon, href }) => (
             <li key={name}>
               <a
                 href={href}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="block p-4 transition-opacity hover:!opacity-100 focus-visible:!opacity-100 group-hover:opacity-50"
+                className="block p-4 transition-opacity hover:!opacity-100 focus-visible:!opacity-100 group-hover:opacity-25 dark:group-hover:opacity-50"
               >
                 <Icon className="size-6" />
               </a>
@@ -197,7 +197,7 @@ export default function Home() {
           ))}
         </ul>
 
-        <p className="mt-auto text-sm text-neutral-200 opacity-50">
+        <p className="mt-auto text-sm text-neutral-600 dark:text-neutral-200 dark:opacity-50">
           🐭 {new Date().getFullYear()}
         </p>
       </main>
