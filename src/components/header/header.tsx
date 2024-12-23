bc1d573682ecd485e6e4783ed4b6b0972382f72f
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-12-23 02:33:44 +0100

    Implement `Header`'s default active tab

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { Logo } from '@/components/logo'
 import { TabsMenu } from '@/components/ui/tabs-menu'
 import cn from '@/utils/cn'
+import { usePathname } from 'next/navigation'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 const getStyles = tv({
@@ -16,47 +18,52 @@ interface HeaderProps extends VariantProps<typeof getStyles> {
 }
 
 const DISABLED_LABEL = 'Coming soon'
+const ITEMS = [
+  {
+    id: 'item-1',
+    label: 'Home',
+    disabled: true,
+    disabledLabel: DISABLED_LABEL,
+  },
+  {
+    id: 'item-2',
+    label: 'About',
+    disabled: true,
+    disabledLabel: DISABLED_LABEL,
+  },
+  {
+    id: 'item-3',
+    label: 'Experience',
+    disabled: true,
+    disabledLabel: DISABLED_LABEL,
+  },
+  {
+    id: 'item-4',
+    label: 'Writings',
+    href: '/writings',
+  },
+  {
+    id: 'item-5',
+    label: 'Contact',
+    disabled: true,
+    disabledLabel: DISABLED_LABEL,
+  },
+]
 
 function Header(props: HeaderProps) {
   const { className = '', ...restProps } = props
 
   const styles = getStyles()
 
+  const pathname = usePathname()
+  const defaultActive = ITEMS.findIndex((item) => item.href === pathname)
+
   return (
     <>
       <TabsMenu
         className={cn(className, styles.container())}
-        items={[
-          {
-            id: 'item-1',
-            label: 'Home',
-            disabled: true,
-            disabledLabel: DISABLED_LABEL,
-          },
-          {
-            id: 'item-2',
-            label: 'About',
-            disabled: true,
-            disabledLabel: DISABLED_LABEL,
-          },
-          {
-            id: 'item-3',
-            label: 'Experience',
-            disabled: true,
-            disabledLabel: DISABLED_LABEL,
-          },
-          {
-            id: 'item-4',
-            label: 'Writings',
-            href: '/writings',
-          },
-          {
-            id: 'item-5',
-            label: 'Contact',
-            disabled: true,
-            disabledLabel: DISABLED_LABEL,
-          },
-        ]}
+        items={ITEMS}
+        defaultActive={defaultActive}
         renderBefore={<Logo color="light" className="mx-3" size="sm" />}
         {...restProps}
       />
