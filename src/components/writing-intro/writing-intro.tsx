e82b81069d36dfcdb66e100a3e2c400487a82f08
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2025-04-26 20:19:49 +0200

    Revert "Preload author image"
    This reverts commit 2d6f0c18b9909e7a30ef657caedd0c841c455561.

--- src/components/writing-intro/writing-intro.tsx
+++ src/components/writing-intro/writing-intro.tsx
@@ -100,7 +100,6 @@ function WritingIntro(props: WritingIntroProps) {
       <div className={styles.author()}>
         <Image
           className={styles.authorImage()}
-          priority
           src="/author-image.jpg"
           width={48}
           height={48}
